d100ef3877bbb56f0c39a2f430990d22198830fa
Mert Cenk Kılıç <kmertcenk@gmail.com>
2021-03-18 20:34:44 +0300

    - merge sort divided to functions - first part of multiprocessing is implemented

--- mergesort/merge_sort.py
+++ mergesort/merge_sort.py
@@ -1,4 +1,34 @@
+import concurrent.futures
+import math
+import multiprocessing
+from multiprocessing import Process, Queue
 import time
+
+
+def merge(L,R,arr):
+    i = j = k = 0
+
+    # veriyi l ve r adında geçici listelere kopyala
+    while i < len(L) and j < len(R):
+        if L[i] < R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    
+    # element kalmış mı diye kontrol et
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
+    return arr
 
 
 def mergeSort(arr):
@@ -9,56 +39,53 @@ def mergeSort(arr):
 
         # dizi elemanlarını 2 parçaya böler
         L = arr[:mid]
-        print(L)
+        #print(L)
         # 2. parça
         R = arr[mid:]
-        print(R)
+        #print(R)
         # ilk yarıyı sırala
         mergeSort(L)
 
         # ikinci yarıyı sırala
         mergeSort(R)
+        merge(L,R,arr)
 
-        i = j = k = 0
+    else:
+        return arr
 
-        # veriyi l ve r adında geçici listelere kopyala
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
-        print(arr)
-        # element kalmış mı diye kontrol et
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
-        print(arr)
 
 # listeyi print et
-
-
 def printList(arr):
     for i in range(len(arr)):
         print(arr[i], end=" ")
     print()
 
 
-# Driver Code
+
+def parallelMergeSort(arr):
+    # processes = multiprocessing.cpu_count()
+    # pool = multiprocessing.Pool(processes=2)
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
+    # p1 = Process(mergeSort, (left))
+    # p1.start()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        f1 = executor.submit(mergeSort, (left, ))
+        f2 = executor.submit(mergeSort, (right, ))
+        result_left = f1.result()
+
+
 if __name__ == '__main__':
     startTime = time.time()
-    arr = [12,22,14,15,123,34,45,56,544,23,34,11,19,87,33]
+    arr = [12, 22, 14, 15, 123, 34, 45, 56, 544, 23, 34, 11, 19, 87, 33]
+    print(arr)
     print("Given array is", end="\n")
     printList(arr)
-    mergeSort(arr)
     print("Sorted array is: ", end="\n")
-    printList(arr)
+    parallelMergeSort(arr)
     print("--- %s seconds ---" % (time.time() - startTime))
+
+
+
+
